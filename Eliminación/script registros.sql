71f49fd6d380d95784ce7fbaf1fa0d7d72118784
josuetipan <josuetipan101@gmail.com>
2024-03-08 21:41:28 -0500

    eliminacion

---
+++ Eliminación/script registros.sql
@@ -0,0 +1,40 @@
+--tabla 4
+
+create table registros_entrada(
+	codigo_registro int not null,
+	cedula_empleado char(10) not null,
+	fecha date not null,
+	hora time not null,
+	constraint registro_pk primary key (codigo_registro)
+)
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101001,'1754772778','01/02/2024','07:00')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101002,'1754772477','08/02/2023','07:00')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101003,'1714788535','01/12/2021','08:00')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101004,'1754772747','02/12/2020','09:00')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101005,'1754772564','01/06/2019','08:00')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101006,'1754772469','01/06/2020','08:00')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101007,'1754264788','03/02/2024','08:30')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101008,'1754698756','08/08/2021','07:00')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101009,'1754772228','05/10/2020','09:00')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101010,'1758444296','06/05/2017','07:00')
+-----------
+select cedula_empleado,fecha,hora from registros_entrada
+select * from registros_entrada where hora 
+between '7:00' and '14:00'
+select * from registros_entrada where hora > '8:00'
+----------------------
+update registros_entrada set cedula_empleado = '082345679'
+where extract(month from fecha)=8
+----------------------------
+delete from registros_entrada
+where extract(month from fecha)=6
---
+++ Eliminación/script transacciones.sql
@@ -0,0 +1,48 @@
+--tabla 6
+
+create table transacciones(
+	codigo int not null,
+	numero_cuenta char(5) not null,
+	monto money not null,
+	tipo char(1) not null,
+	fecha date not null,
+	hora time not null,
+	constraint transacciones_pk primary key (codigo)
+)
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (5478,'12345',500,'C','15/01/2024','12:00')
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (5479,'12115',50,'C','15/01/2024','12:30')
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (5460,'15487',100,'C','15/01/2024','13:00')
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (5434,'57534',1000,'C','15/01/2024','13:30')
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (5453,'12445',3200,'C','15/01/2024','14:00')
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (5477,'65743',1500,'C','15/01/2024','14:30')
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (5445,'12356',2500,'C','15/01/2024','15:00')
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (5471,'12456',1000,'C','15/01/2024','15:30')
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (5448,'12345',5200,'C','15/01/2024','16:00')
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (6547,'45356',3500,'C','15/01/2024','16:30')
+insert into transacciones(codigo,numero_cuenta,monto,tipo,fecha,hora)
+values (0000,'45315',3505,'C','15/08/2024','16:30')
+----------------------
+select * from transacciones where tipo = 'D'
+select * from transacciones where monto between money(200) and money(2000)
+select codigo,monto,tipo,fecha from transacciones where fecha is not null
+-----------------------
+update transacciones set tipo = 'T'
+where monto > money(100) 
+	and monto < money(500)
+	AND EXTRACT(MONTH FROM fecha) = 9
+  	AND EXTRACT(HOUR FROM hora) BETWEEN 14 AND 20
+-------------------
+delete from transacciones
+where extract(hour from hora) between 14 and 18 
+and extract(month from fecha) = 8
+and extract(year from fecha) = 2024
