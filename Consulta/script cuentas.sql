7ec62a4954b2b69906a8c63dbf3da81f03d7edd9
josuetipan <josuetipan101@gmail.com>
2024-03-25 00:02:05 -0500

    consulta de los dos

---
+++ Consulta/script cuentas.sql
@@ -0,0 +1,156 @@
+--tabla numero 2
+
+create table cuentas(
+	numero_cuenta char(5) not null,
+	cedula_propietario char(5) not null,
+	fecha_creación date not null,
+	saldo money not null,
+	constraint cuentas_pk primary key (numero_cuenta)
+)
+
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values ('18064','17547','11/09/2023',100.00)
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values ('20574','17099','02/11/2006',80.70)
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values ('15784','17248','01/01/1973',4859.00)
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values ('18003','17747','06/02/2000',6045.00)
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values ('18004','17864','03/12/2004',100.00)
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values ('18124','07747','16/04/1990',800.00)
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values ('78942','01537','30/10/2002',863.00)
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values ('02147','08647','28/03/2019',2345.00)
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values ('56478','17228','03/06/2017',98.00)
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values ('12503','17117','17/12/2006',100.00)
+----------------------
+select * from cuentas
+select numero_cuenta,saldo from cuentas
+select fecha_creación,* from cuentas where fecha_creación
+between '01/01/2024' and '07/03/2024'
+select numero_cuenta,saldo from cuentas where  fecha_creación
+between '01/01/2024' and '07/03/2024'
+-------------------
+update cuentas set saldo = 10.00
+where cedula_propietario like '17%'
+-------------------------
+delete from cuentas
+where cedula_propietario like '10%'
+--------------------
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (25569,17504,'15/01/2030',500);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (25570,17500,'01/11/2028',600);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (25571,17501,'22/01/2023',700);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (25572,17508,'30/05/2022',800);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (25573,17509,'15/03/2023',900);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (25574,17510,'15/05/2023',1000);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (25575,17507,'15/09/2023',1500);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (25576,17515,'15/10/2024',2500);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (25577,17512,'15/08/2025',3500);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (25578,17503,'15/06/2017',4500)
+------------------------------------------
+select numero_cuenta,saldo,* from cuentas
+where saldo > money(100) and saldo < money(1000)
+select * from cuentas
+where fecha_creación between '01/01/2023'  and '10/03/2024' 
+select * from cuentas 
+where saldo = money(0) or cedula_propietario like '%2'
+----------------------------------------------
+select * from cuentas
+
+delete from cuentas;
+delete from usuario;
+
+create table usuario(
+	
+	cedula char(5),
+	nombre varchar(25) not null,
+	apellido varchar(25) not null,
+	tipo_cuenta varchar(20),
+	limite_credito decimal,
+	constraint usuario_pk primary key (cedula)
+)
+
+alter table cuentas
+add constraint cuentas_usuario_fk
+foreign key (cedula_propietario)
+references usuario(cedula)
+
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (25578,17547,'15/06/2017',150);--
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (85741,21314,'21/08/2022',100);--
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (12457,10257,'15/09/2022',200);--
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (25487,17653,'12/10/2022',300);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (12487,14361,'02/11/2022',400);--
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (00457,12481,'18/12/2022',500);--
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (00217,17781,'11/01/2023',600);--
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (78254,17351,'15/04/2023',700);--
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (45612,17581,'02/07/2023',800);--
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creación,saldo)
+values (54612,17221,'21/09/2023',900);--
+
+
+
+insert into usuario(cedula,nombre,apellido,tipo_cuenta,limite_credito)
+values ('17547','Josue','Tipan','ahorro','5');
+insert into usuario(cedula,nombre,apellido,tipo_cuenta,limite_credito)
+values ('21314','Andre','vinueza','ahorro','6.3');
+insert into usuario(cedula,nombre,apellido,tipo_cuenta,limite_credito)
+values ('10257','Segundo','Casa','corriente','4');
+insert into usuario(cedula,nombre,apellido,tipo_cuenta,limite_credito)
+values ('17653','Miguel','Quishpe','ahorro','3');
+insert into usuario(cedula,nombre,apellido,tipo_cuenta,limite_credito)
+values ('14361','Piedad','Maquis','ahorro','2');--
+insert into usuario(cedula,nombre,apellido,tipo_cuenta,limite_credito)
+values ('12481','Laura','Muquis','corriente','3');--
+insert into usuario(cedula,nombre,apellido,tipo_cuenta,limite_credito)
+values ('17781','Angel','Criollo','ahorro','8');--
+insert into usuario(cedula,nombre,apellido,tipo_cuenta,limite_credito)
+values ('17351','Gladys','Tiuaña','ahorro','6');--
+insert into usuario(cedula,nombre,apellido,tipo_cuenta,limite_credito)
+values ('17581','Sonia','Vastida','corriente','3.2');--
+insert into usuario(cedula,nombre,apellido,tipo_cuenta,limite_credito)
+values ('17221','Guadalupe','Pillajo','ahorro','5.5');--
+------------------------------
+select cu.numero_cuenta, us.nombre from
+cuentas cu,usuario us
+where 
+cu.cedula_propietario = us.cedula 
+and cu.saldo>money(100) and cu.saldo<money(1000)
+
+select cu.numero_cuenta,cu.fecha_creación,cu.saldo,us.cedula from
+cuentas cu,usuario us
+where 
+cu.cedula_propietario = us.cedula 
+and cu.fecha_creación between '21/09/2022' and '21/09/2023'
+
+
+
+
+
+
+
+
+
---
+++ Consulta/script registros.sql
@@ -0,0 +1,177 @@
+--tabla 4
+
+create table registros_entrada(
+	codigo_registro int not null,
+	cedula_empleado char(10) not null,
+	fecha date not null,
+	hora time not null,
+	constraint registro_pk primary key (codigo_registro)
+)
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101001,'1754772778','01/02/2024','07:00')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101002,'1754772477','08/02/2023','07:00')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101003,'1714788535','01/12/2021','08:00')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101004,'1754772747','02/12/2020','09:00')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101005,'1754772564','01/06/2019','08:00')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101006,'1754772469','01/06/2020','08:00')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101007,'1754264788','03/02/2024','08:30')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101008,'1754698756','08/08/2021','07:00')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101009,'1754772228','05/10/2020','09:00')
+insert into registros_entrada (codigo_registro,cedula_empleado,fecha,hora)
+values (101010,'1758444296','06/05/2017','07:00')
+-----------
+select cedula_empleado,fecha,hora from registros_entrada
+select * from registros_entrada where hora 
+between '7:00' and '14:00'
+select * from registros_entrada where hora > '8:00'
+----------------------
+update registros_entrada set cedula_empleado = '082345679'
+where extract(month from fecha)=8
+----------------------------
+delete from registros_entrada
+where extract(month from fecha)=6
+-----------------------------------
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(12345, '1754416489', '18/06/2023', '7:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(12355, '1758254591', '03/08/2023', '14:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(12346, '1715974492', '25/03/2022', '23:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(12347, '1735642583', '28/12/2015', '16:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(12389, '1707416984', '16/07/2012', '8:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(12349, '1778541235', '17/03/2013', '10:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(12310, '1742158637', '14/05/2017', '19:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(12311, '1756321784', '15/05/2004', '20:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(12358, '1712546328', '07/07/2008', '21:00');
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora)
+values(12378, '1725785413', '02/09/2023', '4:00');
+---------------------------------
+select * from registros_entrada
+where extract(month from fecha)= 9 or cedula_empleado like '17%'
+select * from registros_entrada
+where extract(month from fecha) = 8 and cedula_empleado like '17%' and hora 
+between '08:00' and '12:00'
+select * from registros_entrada
+where extract(month from fecha) = 8 and cedula_empleado like '17%' and hora 
+between '08:00' and '12:00' or extract(month from fecha)= 9 and cedula_empleado like '08%' and 
+hora between '09:00' and '13:00'
+------------------------------
+
+create table empleado(
+	codigo_empleado int ,
+	nombre varchar(25) not null,
+	fecha date not null,
+	hora time not null
+)
+
+
+
+alter table registros_entrada
+add column codigo_empleado int 
+
+drop table empleado
+ 
+ALTER TABLE registros_entrada
+ADD CONSTRAINT registro_empleado_fk
+FOREIGN KEY (codigo_empleado)
+REFERENCES empleado (codigo_empleado);
+
+ALTER TABLE empleado
+ADD CONSTRAINT nombre_constraint UNIQUE (codigo_empleado);
+
+select * from registros_entrada
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora,codigo_entrada)
+values(12355, '1758254591', '03/08/2023', '14:00',11);
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora,codigo_entrada)
+values(12346, '1715974492', '25/03/2022', '23:00',12);
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora,codigo_entrada)
+values(12347, '1735642583', '28/12/2015', '16:00',13);
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora,codigo_entrada)
+values(12389, '1707416984', '16/07/2012', '8:00',14);
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora,codigo_entrada)
+values(12349, '1778541235', '17/03/2013', '10:00',15);
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora,codigo_entrada)
+values(12310, '1742158637', '14/05/2017', '19:00',16);
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora,codigo_entrada)
+values(12311, '1756321784', '15/05/2004', '20:00',17);
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora,codigo_entrada)
+values(12358, '1712546328', '07/07/2008', '21:00',18);
+
+insert into registros_entrada(codigo_registro, cedula_empleado, fecha, hora,codigo_entrada)
+values(12378, '1725785413', '02/09/2023', '4:00',19);
+---------------------------------
+
+insert into empleado(codigo_empleado,nombre,fecha,hora)
+values(11,'Acero','11/02/2023','07:00');
+
+insert into empleado(codigo_empleado,nombre,fecha,hora)
+values(12,'ALCIVAR','11/02/2023','07:00');
+
+insert into empleado(codigo_empleado,nombre,fecha,hora)
+values(13,'JOSUE','11/02/2023','07:00');
+
+insert into empleado(codigo_empleado,nombre,fecha,hora)
+values(14,'PEDRO','11/02/2023','07:00');
+
+insert into empleado(codigo_empleado,nombre,fecha,hora)
+values(15,'MATIAS','11/02/2023','07:00');
+
+insert into empleado(codigo_empleado,nombre,fecha,hora)
+values(16,'MAITE','11/02/2023','07:00');
+
+insert into empleado(codigo_empleado,nombre,fecha,hora)
+values(17,'TATIANA','11/02/2023','07:00');
+
+insert into empleado(codigo_empleado,nombre,fecha,hora)
+values(18,'ANDRES','11/02/2023','07:00');
+
+insert into empleado(codigo_empleado,nombre,fecha,hora)
+values(19,'ARIEL','11/02/2023','07:00');
+
+
+select re.cedula_empleado,em.fecha,em.nombre from
+registros_entrada re,empleado em
+where 
+em.fecha between '01/08/2023' and '21/08/2023' or re.cedula_empleado like '%17%'
+and em.hora between '8:00' and '12:00' or em.fecha between '06/10/2023' and '20/10/2023' or 
+re.cedula_empleado like '08%' and em.hora between '9:00' and '13:00'
+
+select re.codigo_registro,em.nombre from
+registros_entrada re,empleado em
+where 
+re.cedula_empleado = '2201'
+
+
+
+
